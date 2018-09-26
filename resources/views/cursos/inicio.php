4e965f8a1be54f5ca79c8d126838a0db59d09b2b
Limber Rojas Villarroel <liroevi@hotmail.com>
2018-09-26 07:11:27 -0400

    Primer repositorio de laravel

---
+++ resources/views/cursos/inicio.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Plantillas</h1>
+    <button type="button" name="button" class="but but">Opcion1</button>
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'pagecontroller@inicio');
+
+Route::get('Bienvenido', function () {
+    return "Hola!";
+});
+
+Route::get('fotos/{numero?}', function ($numero='Sin numero') {
+    return "Estas en la galeria de fotos : ".$numero;
+})->where('numero','[0-9]+');
+
+Route::get('cursos', function(){
+   return view('inicio');
+});
